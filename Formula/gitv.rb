52a2908e222ecdc60e87f5a6e500b23e72495d77
JayanAXHF <sunil.chdry@gmail.com>
2026-02-26 20:19:48 +0530

    gitv 0.3.2

--- Formula/gitv.rb
+++ Formula/gitv.rb
@@ -1,9 +1,9 @@
 class Gitv < Formula
   desc "Terminal-based GitHub client built with Rust and Ratatui"
   homepage "https://github.com/JayanAXHF/gitv"
-  url "https://github.com/JayanAXHF/gitv/archive/refs/tags/gitv-tui-v0.3.1.tar.gz"
-  version "0.3.1"
-  sha256 "a6b6869537c033392a860ba60ce2e2878c2b0f6b7f760f30e3c54a0e5d0ee862"
+  url "https://github.com"
+  version "0.3.2"
+  sha256 "ff339439765f6b061f198f4c94070ffb7b255137fe7563deb0ff56e7e3272d03"
   license any_of: ["Unlicense", "MIT"]
 
   depends_on "rust" => :build
